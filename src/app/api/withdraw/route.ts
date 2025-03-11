a0d7039da8d6d94b729289d9a586fa38c6ae9919
SHIAK AFRID <afridayan01@gmail.com>
2025-03-11 10:10:22 +0000

    Updated route

--- src/app/api/withdraw/route.ts
+++ src/app/api/withdraw/route.ts
@@ -1,3 +1,4 @@
+import { responseEnums } from "@/app/enums/responseEnums";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -7,48 +8,39 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 
 export async function POST(req: Request) {
   try {
-    // const account = await stripe.accounts.create({
-    //   type: "express",
-    //   email: "afridayan01@gmail.com",
-    //   country: "US",
-    //   capabilities: {
-    //     transfers: { requested: true },
-    //     card_payments: {
-    //       requested: true,
-    //     },
-    //   },
-    //   business_type: "individual",
-    //   default_currency: "USD",
-    // });
-    const accountLink = await stripe.accountLinks.create({
-      account: "acct_1R11TzCRwSUYOPKq",
-      refresh_url: "http://127.0.0.1:5173/kyc",
-      return_url: "http://127.0.0.1:5173/myprofile",
-      type: "account_onboarding",
+    const transfer = await stripe.transfers.create({
+      amount: 100000, 
+      currency: "usd",
+      destination: "acct_1R0qRlCAdtgfHNQO", 
+      description: "Transfer to freelancer for milestone payment",
     });
-    // const transfer = await stripe.transfers.create({
-    //   amount: 100000, // Amount in cents (e.g., 1000 for $10.00)
-    //   currency: "usd",
-    //   destination: "acct_1R0qRlCAdtgfHNQO", // Freelancer's connected account ID
-    //   description: "Transfer to freelancer for milestone payment",
-    // });
-    // const payout = await stripe.payouts.create(
-    //   {
-    //     amount: 1000, // Amount in cents (e.g., 1000 for $10.00)
-    //     currency: "usd", // Adjust currency as needed
-    //   },
-    //   {
-    //     stripeAccount: "acct_1R0qRlCAdtgfHNQO",
-    //   }
-    // );
+    const payout = await stripe.payouts.create(
+      {
+        amount: 1000,
+        currency: "usd", 
+      },
+      {
+        stripeAccount: "acct_1R0qRlCAdtgfHNQO",
+      }
+    );
 
-    return NextResponse.json({
-      a: accountLink?.url,
-    });
+    return NextResponse.json(
+      {
+        mesasge: responseEnums?.SUCCESS,
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (error: any) {
     console.log(error);
-    return NextResponse.json("messag");
+    return NextResponse.json(
+      {
+        mesasge: responseEnums?.ERROR,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
-
-
